9d4732b37cf7b3a143eed63a59c007d33af5c175
ameijer <alex.t.meijer@gmail.com>
2016-01-21 08:21:07 -0500

    finished user adding utility, ready to add new fake users

--- Backend/utils/src/main/java/utils/LoginRegisterThread.java
+++ Backend/utils/src/main/java/utils/LoginRegisterThread.java
@@ -26,123 +26,133 @@ import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 
 public class LoginRegisterThread extends Thread {
-    private String mEmail;
-    private String mUsername;
-    private String mPassword;
-    private String oAuth;
-    private final boolean isRegister;
-    String server = "http://104.131.81.78";
-    int port = 10004;
+	private String mEmail;
+	private String mUsername;
+	private String mPassword;
+	private String oAuth;
+	private final boolean isRegister;
+	String server = "http://104.131.81.78";
+	int port = 10004;
 
-    private final Point location;
-    private User mNoob;
-    private LoginResponse loginResponse;
-    private final String type;
+	private final Point location;
+	private User mNoob;
+	private LoginResponse loginResponse;
+	private final String type;
 	private String id;
 
-    public enum LoginTypes {
-        facebook, nostalgia, google
-    }
+	public enum LoginTypes {
+		facebook, nostalgia, google
+	}
 
-    public LoginRegisterThread(String email, String uname, String pass, String type, boolean isRegister, Point location, String string) {
-        mEmail = email;
-        mUsername = uname;
-        mPassword = pass;
-        this.type = type;
-        this.isRegister = isRegister;
-        this.id = string;
-       
-        this.location = location;
+	public LoginRegisterThread(String email, String uname, String pass, String type, boolean isRegister, Point location, String string) {
+		mEmail = email;
+		mUsername = uname;
+		mPassword = pass;
+		this.type = type;
+		this.isRegister = isRegister;
+		this.id = string;
 
-    }
+		this.location = location;
 
+	}
 
-    public void run() {
-        mNoob = new User();
-        mNoob.setEmail(mEmail);
-        mNoob.setName(mUsername);
-        mNoob.setToken(oAuth);
-        mNoob.set_id(id);
-        mNoob.setLastKnownLoc(location);
-        ArrayList<String> devices = new ArrayList<String>();
-        devices.add("39bb3308-f1de-4794-bd12-1b05aecafba5");
-        mNoob.setAuthorizedDevices(devices);
-        if(mPassword != null) {
-            mNoob.setPassword(new String(Hex.encodeHex(DigestUtils.sha512(mPassword))));
-        }
-        
-        System.out.println("registering new user: " + mNoob.getName());
-
-        ObjectMapper mapper = new ObjectMapper();
-        try {
-            System.out.println(mapper.writeValueAsString(mNoob));
-        } catch (JsonProcessingException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        HttpPost httpPost = null;
-        if(isRegister){
-            httpPost = new HttpPost(server + ":" + port + "/api/v0/user/register?type=" + type);
-        } else {
-            httpPost = new HttpPost(server + ":" + port + "/api/v0/user/login?type=" + type);
-        }
-        try {
-            httpPost.setEntity(new StringEntity(mapper.writeValueAsString(mNoob)));
-        } catch (UnsupportedEncodingException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (JsonProcessingException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        httpPost.setHeader("Accept", "application/json");
-        httpPost.setHeader("Content-type", "application/json");
+	public LoginRegisterThread(User toAdd, boolean isRegister, String type) {
+		mNoob = toAdd; 
+		this.isRegister = isRegister;
+		location = null;
+		this.type = type;
+	}
 
 
-        HttpResponse resp = null;
-        try {
-            resp = new DefaultHttpClient().execute(httpPost);
-        } catch (ClientProtocolException e1) {
-            // TODO Auto-generated catch block
-            e1.printStackTrace();
-        } catch (IOException e1) {
-            // TODO Auto-generated catch block
-            e1.printStackTrace();
-        }
+	public void run() {
+		if(mNoob == null){
+			mNoob = new User();
+			mNoob.setEmail(mEmail);
+			mNoob.setName(mUsername);
+			mNoob.setToken(oAuth);
+			mNoob.set_id(id);
+			mNoob.setLastKnownLoc(location);
+		}
+		ArrayList<String> devices = new ArrayList<String>();
+		devices.add("39bb3308-f1de-4794-bd12-1b05aecafba5");
+		mNoob.setAuthorizedDevices(devices);
+
+		if(mPassword != null) {
+			mNoob.setPassword(new String(Hex.encodeHex(DigestUtils.sha512(mPassword))));
+		}
+
+		System.out.println("registering new user: " + mNoob.getName());
+
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			System.out.println(mapper.writeValueAsString(mNoob));
+		} catch (JsonProcessingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		HttpPost httpPost = null;
+		if(isRegister){
+			httpPost = new HttpPost(server + ":" + port + "/api/v0/user/register?type=" + type);
+		} else {
+			httpPost = new HttpPost(server + ":" + port + "/api/v0/user/login?type=" + type);
+		}
+		try {
+			httpPost.setEntity(new StringEntity(mapper.writeValueAsString(mNoob)));
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonProcessingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		httpPost.setHeader("Accept", "application/json");
+		httpPost.setHeader("Content-type", "application/json");
 
 
-        try {
-            if(resp.getEntity()!= null) {
-                String contents = IOUtils.toString(resp.getEntity().getContent(), "UTF-8");
-                loginResponse = mapper.readValue(contents, LoginResponse.class);
-            }
+		HttpResponse resp = null;
+		try {
+			resp = new DefaultHttpClient().execute(httpPost);
+		} catch (ClientProtocolException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 
-        } catch (JsonParseException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (JsonMappingException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (IllegalStateException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (NullPointerException e) {
-            e.printStackTrace();
-        }
 
-        if(loginResponse != null){
-            System.out.println( "User registered!");
-        } else {
-        	System.out.println(  "User NOT registered!");
-        }
-    }
+		try {
+			if(resp.getEntity()!= null) {
+				String contents = IOUtils.toString(resp.getEntity().getContent(), "UTF-8");
+				loginResponse = mapper.readValue(contents, LoginResponse.class);
+			}
 
-    public LoginResponse getLoginResponse() {
-        return loginResponse;
-    }
+		} catch (JsonParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalStateException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NullPointerException e) {
+			e.printStackTrace();
+		}
+
+		if(loginResponse != null){
+			System.out.println( "User registered!");
+		} else {
+			System.out.println(  "User NOT registered!");
+		}
+	}
+
+	public LoginResponse getLoginResponse() {
+		return loginResponse;
+	}
 }
 
 
--- Backend/utils/src/main/java/utils/UserAdder.java
+++ Backend/utils/src/main/java/utils/UserAdder.java
@@ -1,10 +1,13 @@
 package utils;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -13,7 +16,9 @@ import java.util.Map;
 import java.util.Scanner;
 import java.util.UUID;
 
+import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.geojson.Feature;
 import org.geojson.Point;
 
@@ -30,6 +35,7 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.nostalgia.persistence.model.KnownLocation;
+import com.nostalgia.persistence.model.LoginResponse;
 import com.nostalgia.persistence.model.User;
 import com.nostalgia.persistence.model.Video;
 
@@ -39,7 +45,7 @@ public class UserAdder {
 
 
 
-	public static void main(String[] args) throws IOException, InterruptedException{
+	public static void main(String[] args) throws Exception{
 		Scanner scanner = new Scanner(System.in);
 		boolean running = true;
 		File operatingDir = new File(System.getProperty("user.dir"));
@@ -68,7 +74,7 @@ public class UserAdder {
 				System.out.println("initing db...");
 				setupDB();
 				System.out.println("db inited");
-				
+
 				for(User scanned : scannedUsers){
 					System.out.println("Checking for existence of user: " + scanned.getName());
 					if(bucket.get(scanned.get_id()) != null){
@@ -78,126 +84,149 @@ public class UserAdder {
 				}
 
 			}
-		
 
-		System.out.println("Users found: ");
 
-		for(int i = 0; i < scannedUsers.size(); i++){
-			User cur = scannedUsers.get(i);
-		
-			System.out.println(i + ": USer name: " + scannedUsers.get(i).getName());
-			System.out.println("      with id: " + scannedUsers.get(i).get_id());
-			System.out.println("      created on: " + scannedUsers.get(i).getDateJoined());
-			System.out.println();
-		}
+			System.out.println("Users found: ");
 
-		if(scannedUsers.size() == 0){
-			System.out.println("None.");
-		}
-		System.out.println("\n");
-		System.out.print("add which user #? (q to quit, e for example gen): ");
-		String selection = null;
+			for(int i = 0; i < scannedUsers.size(); i++){
+				User cur = scannedUsers.get(i);
 
-		selection = scanner.nextLine();
-		int asNum = -1;
-		try {
-			asNum = Integer.parseInt(selection);
-		} catch (Exception e){
-			switch(selection){
+				System.out.println(i + ": USer name: " + scannedUsers.get(i).getName());
+				System.out.println("      with id: " + scannedUsers.get(i).get_id());
+				System.out.println("      created on: " + scannedUsers.get(i).getDateJoined());
+				System.out.println();
+			}
 
-			case("q"):
-				System.out.println("Goodbye");
-			System.exit(0);
-			break;
+			if(scannedUsers.size() == 0){
+				System.out.println("None.");
+			}
+			System.out.println("\n");
+			System.out.print("add which user #? (q to quit, e for example gen): ");
+			String selection = null;
 
-			case("e"):
-				generateExampleJsonForUser(userJsonDir);
-			continue;
+			selection = scanner.nextLine();
+			int asNum = -1;
+			try {
+				asNum = Integer.parseInt(selection);
+			} catch (Exception e){
+				switch(selection){
 
-			default:
-				System.out.println("Error - command not recognized: " + selection);
-				Thread.sleep(150);
+				case("q"):
+					System.out.println("Goodbye");
+				System.exit(0);
+				break;
+
+				case("e"):
+					generateExampleJsonForUser(userJsonDir);
 				continue;
+
+				default:
+					System.out.println("Error - command not recognized: " + selection);
+					Thread.sleep(150);
+					continue;
+				}
+			}
+
+			User toAdd = scannedUsers.get(asNum);
+
+			System.out.println("Adding user: " + toAdd.getName());
+
+			if(toAdd.getIcon() == null || toAdd.getIcon().length() < 10){
+				toAdd.setIcon(null);
+				System.out.println("no encoded icon found in json for: " + toAdd.getName());
+				System.out.print("add local icon to user? (y/[n])(answering no will cause a default icon to be generated server side): ");
+
+				String answer = scanner.nextLine();
+				if(answer == null || answer.equals("") || answer.contains("n")){
+					System.out.println("skipping photo add.");
+
+				} else {
+
+					System.out.println("Enter the path to the users's icon, or [ENTER] to search in: " + userPicDir.getAbsolutePath());
+					System.out.println("Note - the data will be copied into the data dir if alternate path specified");
+
+					String path = scanner.nextLine();
+
+					String searchPath = null;
+
+					if(path.length() < 2){
+						System.out.println("Searching in: " + userPicDir.getAbsolutePath() + " for file: " + toAdd.get_id());
+						searchPath = userPicDir.getAbsolutePath() + "/" + toAdd.get_id() + ".png";
+
+					} else {
+						System.out.println("Searching in: " + path);
+						searchPath = path;
+					}
+
+					File data = new File(searchPath);
+					if(!data.exists()){
+						System.err.println("Error - no file found at: " + path);
+						continue;
+					}
+
+					//we have a data file we know exists 
+
+					System.out.println("File found @" + data.getAbsolutePath());
+
+					File saved = new File(userPicDir, toAdd.get_id() + ".png");
+					if(!data.getAbsolutePath().contains(userPicDir.getName())){
+						//copy
+						System.out.println("Copying file...");
+						FileUtils.copyFile(data, saved);
+						FileInputStream fileBytes = new FileInputStream(saved); 
+						
+						byte[] defaultEncoded = IOUtils.toByteArray(fileBytes);
+						System.out.println("encoding to base64 and attaching to user");
+						toAdd.setIcon(new String(Base64.encodeBase64(defaultEncoded, false, false)));
+						
+						System.out.println("done");
+					}
+				}
+			}
+
+			System.out.print("add user created videos(y/n)? " );
+			String tagAns = scanner.nextLine();
+
+			if(tagAns.contains("y") || tagAns.contains("Y")){
+				addUserCreatedVideos(toAdd);
+			} else {
+				System.out.println("Not adding any user created videos");
+			}
+			System.out.println();
+
+
+
+			FileWriter writer = new FileWriter(new File(userJsonDir, toAdd.get_id() + ".json"));
+
+			String videoAsString = mapper.writeValueAsString(toAdd);
+
+			writer.write(videoAsString);
+			writer.flush();
+			writer.close();
+
+
+			System.out.println("Beginning user registration");
+			LoginRegisterThread task = new LoginRegisterThread(toAdd, true, "app");
+			task.start();
+			task.join();
+
+			LoginResponse resp = task.getLoginResponse();
+			
+			if(resp.getSessionTok() != null){
+			System.out.println("video uploaded successfully");
+			} else {
+				System.err.println("error registering user");
 			}
 		}
-
-		User toAdd = scannedUsers.get(asNum);
-
-		System.out.println("Adding user: " + toAdd.getName());
-
-		System.out.println("Enter the path to the users's icon, or [ENTER] to search in: " + userPicDir.getAbsolutePath());
-		System.out.println("Note - the data will be copied into the data dir if alternate path specified");
-
-		String path = scanner.nextLine();
-
-		String searchPath = null;
-
-		if(path.length() < 2){
-			System.out.println("Searching in: " + userPicDir.getAbsolutePath() + " for file: " + toAdd.get_id());
-			searchPath = userPicDir.getAbsolutePath() + "/" + toAdd.get_id() + ".png";
-
-		} else {
-			System.out.println("Searching in: " + path);
-			searchPath = path;
-		}
-
-		File data = new File(searchPath);
-		if(!data.exists()){
-			System.err.println("Error - no file found at: " + path);
-			continue;
-		}
-
-		//we have a data file we know exists 
-
-		System.out.println("File found @" + data.getAbsolutePath());
-
-		File saved = new File(userPicDir, toAdd.get_id() + ".png");
-		if(!data.getAbsolutePath().contains(userPicDir.getName())){
-			//copy
-			System.out.println("Copying file...");
-			FileUtils.copyFile(data, saved);
-			System.out.println("done");
-		}
-
-		System.out.print("add user created videos(y/n)? " );
-		String tagAns = scanner.nextLine();
-
-		if(tagAns.contains("y") || tagAns.contains("Y")){
-			addUserCreatedVideos(toAdd);
-		} else {
-			System.out.println("Not adding any user created videos");
-		}
-		System.out.println();
-		
-		System.out.println();
-		
-
-		FileWriter writer = new FileWriter(new File(videoJsonDir, toAdd.get_id() + ".json"));
-
-		String videoAsString = mapper.writeValueAsString(toAdd);
-
-		writer.write(videoAsString);
-		writer.flush();
-		writer.close();
-
-
-		System.out.println("Beginning video upload");
-		VideoUploadTask task = new VideoUploadTask(saved.getAbsolutePath(), toAdd);
-		task.start();
-		task.join();
-
-		System.out.println("video uploaded successfully");
-
+		scanner.close();
 	}
-	scanner.close();
-}
 
-private static void addUserCreatedVideos(User toAdd) {
+	private static void addUserCreatedVideos(User toAdd) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-private static void removeUserFrom(ArrayList<User> scannedUsers, User scanned) {
+	private static void removeUserFrom(ArrayList<User> scannedUsers, User scanned) {
 		for(int i = 0; i < scannedUsers.size(); i++){
 			if(scannedUsers.get(i).get_id().equals(scanned.get_id())){
 				//this is the one to remove
@@ -205,112 +234,112 @@ private static void removeUserFrom(ArrayList<User> scannedUsers, User scanned) {
 				return; 
 			}
 		}
-		
+
 		System.err.println("no matching user found to remove");
-		
+
 	}
 
-// the DB we are using
-private static Cluster cluster; 
-private static  Bucket bucket; 
-private static  CouchbaseConfig config ;
-private static BucketManager bucketManager;
+	// the DB we are using
+	private static Cluster cluster; 
+	private static  Bucket bucket; 
+	private static  CouchbaseConfig config ;
+	private static BucketManager bucketManager;
 
-private static void setupDB() {
-	config = new CouchbaseConfig();
-	cluster = CouchbaseCluster.create(config.host);
-	bucket = cluster.openBucket(config.bucketName, config.bucketPassword);
-	bucketManager = bucket.bucketManager();
+	private static void setupDB() {
+		config = new CouchbaseConfig();
+		cluster = CouchbaseCluster.create(config.host);
+		bucket = cluster.openBucket(config.bucketName, config.bucketPassword);
+		bucketManager = bucket.bucketManager();
 
-}
+	}
 
-final static ObjectMapper mapper = new ObjectMapper();
-static{
-	mapper.enable(SerializationFeature.INDENT_OUTPUT);
-}
+	final static ObjectMapper mapper = new ObjectMapper();
+	static{
+		mapper.enable(SerializationFeature.INDENT_OUTPUT);
+	}
 
-private static void generateExampleJsonForUser(File userJsonDir) throws IOException {
-	File outputFile = new File(userJsonDir, "exampleUser.json");
-	userJsonDir.mkdirs();
-	if(outputFile.exists()){
-		System.out.println("example already exists @ " + outputFile.getAbsolutePath());
-		System.out.println("no changes made");
+	private static void generateExampleJsonForUser(File userJsonDir) throws IOException {
+		File outputFile = new File(userJsonDir, "exampleUser.json");
+		userJsonDir.mkdirs();
+		if(outputFile.exists()){
+			System.out.println("example already exists @ " + outputFile.getAbsolutePath());
+			System.out.println("no changes made");
+			return; 
+		} else {
+			outputFile.createNewFile();
+		}
+
+		User example = new User();
+		example.set_id("example_id");
+		example.setName("Example User");
+		example.setPassword("<filled Serverside>");
+
+		example.setPasswordChangeDate(-1);
+		example.setEmail("example@example.com");
+		example.setSettings(new HashMap<String, String>());
+		example.getSettings().put("sync", "always");
+		example.getSettings().put("sharing_who", "EVERYONE");
+		example.getSettings().put("sharing_when", "WIFI");
+		example.getSettings().put("sharing_where", "EVERYWHERE");
+		example.getSettings().put("video_sound", "MUTE");
+		FileWriter writer = new FileWriter(outputFile);
+
+		String videoAsString = mapper.writeValueAsString(example);
+
+		writer.write(videoAsString);
+		writer.flush();
+		writer.close();
+
 		return; 
-	} else {
-		outputFile.createNewFile();
 	}
 
-	User example = new User();
-	example.set_id("example_id");
-	example.setName("Example User");
-	example.setPassword("<filled Serverside>");
-	
-	example.setPasswordChangeDate(-1);
-	example.setEmail("example@example.com");
-	example.setSettings(new HashMap<String, String>());
-	example.getSettings().put("sync", "always");
-	example.getSettings().put("sharing_who", "EVERYONE");
-	example.getSettings().put("sharing_when", "WIFI");
-	example.getSettings().put("sharing_where", "EVERYWHERE");
-	example.getSettings().put("video_sound", "MUTE");
-	FileWriter writer = new FileWriter(outputFile);
+	private static ArrayList<User> scanForUserFilesIn(File userJsonDir) throws Exception {
+		// set date created where needed and make sure ids are unique
+		ArrayList<User> users = new ArrayList<User>();
+		HashMap<String, Long> ids = new HashMap<String, Long>();
+		ids.put("example_id", 0L);
 
-	String videoAsString = mapper.writeValueAsString(example);
+		String[] extensions = new String[]{"json"};
+		Iterator<File> iter = FileUtils.iterateFiles(userJsonDir, extensions, true);
 
-	writer.write(videoAsString);
-	writer.flush();
-	writer.close();
+		ObjectMapper mapper = new ObjectMapper();
 
-	return; 
-}
+		boolean changed = false; 
+		while(iter.hasNext()){
+			File toProcess= iter.next();
+			if(toProcess.getName().equals("exampleUser.json")) {
+				continue;
+			}
+			User fileContents = null;
+			try {
+				fileContents = mapper.readValue(toProcess, User.class);
+			} catch (Exception e) {
+				System.err.println("error reading in file: " + toProcess.getName());
+				e.printStackTrace();
+				continue;
+			}
 
-private static ArrayList<User> scanForUserFilesIn(File userJsonDir) throws Exception {
-	// set date created where needed and make sure ids are unique
-	ArrayList<User> users = new ArrayList<User>();
-	HashMap<String, Long> ids = new HashMap<String, Long>();
-	ids.put("example_id", 0L);
+			//set date created if necessary 
+			if(fileContents.getDateJoined()< 1000000){
+				System.out.println("No date joined found. creating one...");
+				changed = true;
+				fileContents.setDateJoined(System.currentTimeMillis());
 
-	String[] extensions = new String[]{"json"};
-	Iterator<File> iter = FileUtils.iterateFiles(userJsonDir, extensions, true);
+			}
 
-	ObjectMapper mapper = new ObjectMapper();
+			//check id
+			String id = fileContents.get_id();
 
-	boolean changed = false; 
-	while(iter.hasNext()){
-		File toProcess= iter.next();
-		if(toProcess.getName().equals("exampleUser.json")) {
-			continue;
+			if(ids.keySet().contains(id) && !id.contains("example")){
+				System.out.println("duplicate id found.");
+				throw new Exception("DUPLICATE IDS: " + id);
+
+			}
+
+			ids.put(id, System.currentTimeMillis());
+			users.add(fileContents);
 		}
-		User fileContents = null;
-		try {
-			fileContents = mapper.readValue(toProcess, User.class);
-		} catch (Exception e) {
-			System.err.println("error reading in file: " + toProcess.getName());
-			e.printStackTrace();
-			continue;
-		}
-
-		//set date created if necessary 
-		if(fileContents.getDateJoined()< 1000000){
-			System.out.println("No date joined found. creating one...");
-			changed = true;
-			fileContents.setDateJoined(System.currentTimeMillis());
-
-		}
-
-		//check id
-		String id = fileContents.get_id();
-
-		if(ids.keySet().contains(id) && !id.contains("example")){
-			System.out.println("duplicate id found.");
-			throw new Exception("DUPLICATE IDS: " + id);
-
-		}
-
-		ids.put(id, System.currentTimeMillis());
-		users.add(fileContents);
+		return users; 
 	}
-	return users; 
-}
 
 }
